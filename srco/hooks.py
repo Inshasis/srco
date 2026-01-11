e6f69a3272ac3f7fd00df3aaf6c03254812369df
InshaSiS Technologies <136191740+Inshasis@users.noreply.github.com>
2026-01-11 14:52:05 +0530

    Block and Shelf Isssue Fixing

--- srco/hooks.py
+++ srco/hooks.py
@@ -43,7 +43,11 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Address" : "public/js/address.js"}
+doctype_js = {
+    "Address" : "public/js/address.js",
+    "Delivery Note" : "public/js/delivery_note.js",
+    "Purchase Receipt" : "public/js/purchase_receipt.js"
+}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
